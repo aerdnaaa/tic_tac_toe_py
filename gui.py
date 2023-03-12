a4ae697081b8bb7319633b0e508d4f6f476f1516
Andre_pc <andregohkr@hotmaill.com>
2023-03-12 21:02:03 +0800

    Fixed todo, disabled buttons after clicked

--- gui.py
+++ gui.py
@@ -16,26 +16,34 @@ class App(customtkinter.CTk):
             self.btn_list.append(customtkinter.CTkButton(master=self, width=100, height=100, text="", command=lambda index=index: self.button_callback(index)))
             self.btn_list[index].grid(row=int(index/3), column=int(index%3), padx=5, pady=5)
 
+        
+
     def button_callback(self, index):
-        game, row, col, activated_btn = self.game, int(index/3), int(index%3), self.btn_list[index]
+        game, row, col, activated_btn = self.game, int(index/3), int(index%3), self.btn_list[index]        
         if activated_btn.cget("text") == "":
             activated_btn.configure(text="X")
-            game.add_letter_to_board(f"{row} {col}", "X")
-            if game.win(game.board) == "X":
-                # TODO: pop up player 1 won, something wrong with logic p1 appears to win eventhough p2 won
-                print("p1 won")
-            ai_pos = game.find_best_move()
-            print(ai_pos)
-            ai_row, ai_col = game.split_position(ai_pos)
-            ai_btn = self.btn_list[ai_row*3 + ai_col]
-            ai_btn.configure(text="O")
-            game.add_letter_to_board(f"{ai_row} {ai_col}", "X")
-            if game.win(game.board) == "O":
-                # TODO: pop up player 2 won
-                print("p2 won")
+            activated_btn.configure(state="disabled")
+            game.add_letter_to_board(f"{row} {col}", "X")            
+            if game.win(game.board):                
+                self.end_game_text(game.win(game.board))
+            else:
+                ai_pos = game.find_best_move()                
+                ai_row, ai_col = game.split_position(ai_pos)
+                ai_btn = self.btn_list[ai_row*3 + ai_col]
+                ai_btn.configure(text="O")
+                ai_btn.configure(state="disabled")
+                game.add_letter_to_board(f"{ai_row} {ai_col}", "O")
+                if game.win(game.board):                
+                    self.end_game_text(game.win(game.board))
+
+    def end_game_text(self, player):
+        text = ""        
+        if player in [1, 2]:
+            text = f"Player {player} has won!"
         else:
-            # TODO: return error message?
-            pass
+            text = "Tie!"
+        label = customtkinter.CTkLabel(master=self, text=text)
+        label.grid(row=3,column=0, columnspan=3, padx=5, pady=5)
 
 if __name__ == "__main__":
     app = App()
